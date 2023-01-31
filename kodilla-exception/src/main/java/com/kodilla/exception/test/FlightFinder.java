d74b96084d6c03b6369c5f5d5277094910bdbd8b
Jacek Rozdolski <jacekrozdolski@gmail.com>
2023-01-31 19:26:36 +0100

    8.4 Task: exceptions and flight finder

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,35 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightFinder {
+    public boolean findFlight(Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> flightMap = new HashMap<>();
+        flightMap.put("JFK", true);
+        flightMap.put("LAX", true);
+        flightMap.put("FRA", false);
+        flightMap.put("WAW", true);
+        flightMap.put("ARN", false);
+        flightMap.put("WRO", true);
+
+        if (flightMap.containsKey(flight.getArrivalAirport())){
+            return flightMap.get(flight.getArrivalAirport());
+        } else {
+            throw new RouteNotFoundException();
+        }
+    }
+
+    public static void main(String[] args) {
+        FlightFinder flightFinder = new FlightFinder();
+        try {
+            if (flightFinder.findFlight(new Flight("WAW", "RZE"))) {
+                System.out.println("Arrival airport is available");
+            } else {
+                System.out.println("Arrival airport is not available");
+            }
+        } catch (RouteNotFoundException e) {
+            System.out.println("Sorry! Route you are looking for could not be found!");
+        }
+    }
+}
