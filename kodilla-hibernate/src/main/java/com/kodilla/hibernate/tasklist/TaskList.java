ecfb55475f4c522b31cec40bfff513487a84d7c3
Jacek Rozdolski <jacekrozdolski@gmail.com>
2023-03-08 16:51:33 +0100

    13.3 Task: invoice headings and their items

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,17 +1,22 @@
-package com.kodilla.hibernate.tasklist;
+package com.kodilla.hibernate.task;
 
 
 import jakarta.persistence.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
-@Table(name = "TASKLISTS")
+@Table(name="TASKLISTS")
 public class TaskList {
+
     private int id;
     private String listName;
     private String description;
+    private List<Task> tasks = new ArrayList<>();
 
-    public TaskList() {
+    public TaskList(String toDo_tasks) {
     }
 
     public TaskList(String listName, String description) {
@@ -20,14 +25,15 @@ public class TaskList {
     }
 
     @Id
-    @GeneratedValue
     @NotNull
-    @Column(name = "ID", unique = true)
+    @GeneratedValue
+    @Column(name="ID", unique=true)
     public int getId() {
         return id;
     }
 
-    @Column(name = "LISTNAME")
+    @NotNull
+    @Column(name="LISTNAME")
     public String getListName() {
         return listName;
     }
@@ -37,15 +43,29 @@ public class TaskList {
         return description;
     }
 
-    public void setId(int id) {
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    private void setId(int id) {
         this.id = id;
     }
 
-    public void setListName(String listName) {
+    private void setListName(String listName) {
         this.listName = listName;
     }
 
-    public void setDescription(String description) {
+    private void setDescription(String description) {
         this.description = description;
+    }
+
+    private void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
