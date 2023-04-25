295f0018159a80c06d2560d531ee4bd06ffb821d
Ster2 <jacekrozdolski@gmail.com>
2023-04-25 19:28:37 +0200

    20.3 Task: We add logging requests to the facade.

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
@@ -3,7 +3,9 @@ package com.kodilla.patterns2.decorator.pizza;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import static org.junit.Assert.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class PizzaOrderTestSuite {
 
